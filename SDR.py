2779e64c96ae1afb8287f9fe61dc28e525bd32e8
cfan310 <cfanning43@gmail.com>
2024-01-13 23:06:19 -0500

    added numpy result

--- SDR.py
+++ SDR.py
@@ -10,12 +10,4 @@ result = np.square(arr1)
 
 print(result)
 
-# IP address using np.raise to a power of num of items in the array
-
-# first give a subnet
-# then iterate through subnet
-# if first three are 169, print APIPA
-# if first octet is 10, 172 or 192, assign it to a subnetwork
-# automatic SDR subnetting based on given frequencies and locations
-
-ipaddress1 = np.array([])
+# add numpy filter for imported array
