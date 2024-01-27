7d1e763a3c9a4d4a58c4e3b7b2bf843de0287738
cfan310 <cfanning43@gmail.com>
2024-01-27 10:37:44 -0500

    added main function to declare sdr initial frequencies

--- SDR.py
+++ SDR.py
@@ -2,8 +2,26 @@
 
 # Realtek RTL2832U
 
-import numpy as np
+# RTL-SDR Blog V3 R860 RTL2832U 1PPM TCXO HF Bias Tee SMA Software Defined Radio
 
+import numpy as np
+from rtlsdr import RtlSdr
+
+
+# numpy > numerical python (provide mathematical operations on arrays)
+
+def main():
+    sdr = RtlSdr()
+
+    # device configuration
+    sdr.sample_rate = 2.048e6   # set initially to 2,480,000
+
+
+
+
+
+
+'''
 arr1 = np.array([2, 4, 6, 8, 10])
 
 result = np.square(arr1)
@@ -21,3 +39,4 @@ arr2 = np.array([128, 64, 32, 16, 8, 4, 2, 1])
 result = np.square(arr2)
 
 print(result)
+'''
